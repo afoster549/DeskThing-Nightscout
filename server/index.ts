0f1b9771ade448491ffd24680799ed484be8749e
123-Game <aefoster74@gmail.com>
2026-09-10 12:36:27 +0100

    Added simple polling

--- server/index.ts
+++ server/index.ts
@@ -1,25 +1,39 @@
 import { DeskThing } from "@deskthing/server";
 import { DESKTHING_EVENTS } from "@deskthing/types";
 import { setupSettings } from "./settings";
+import { parseNightscoutConfig } from "./formatter";
+import { NightscoutPoller } from "./poller";
+import { FormattedReading } from "./types";
+
+const poller = new NightscoutPoller((reading: FormattedReading) => {
+    const deltaStr = reading.displayDelta ? ` (${reading.displayDelta})` : "";
+    console.log(
+        `[Nightscout] ${reading.displayValue} ${reading.displayUnits} ${reading.arrow}${deltaStr}`
+    );
+});
 
 const start = async () => {
-	await setupSettings();
+    await setupSettings();
 
-	const currentSettings = await DeskThing.getSettings();
-	console.log("Loaded settings on startup:", currentSettings);
+    const currentSettings = await DeskThing.getSettings();
+    console.log("Loaded settings on startup:", currentSettings);
+
+    const config = parseNightscoutConfig(currentSettings);
+    poller.start(config);
 };
 
 const stop = async () => {
-	console.log("Stopped the server")
+    console.log("Stopping Nightscout server...");
+    poller.stop();
 };
 
 DeskThing.on(DESKTHING_EVENTS.SETTINGS, (data) => {
     const updatedSettings = data.payload;
     console.log("Settings changed:", updatedSettings);
 
-    // Reconfigure polling or update API headers with updatedSettings
+    const config = parseNightscoutConfig(updatedSettings);
+    poller.restart(config);
 });
 
 DeskThing.on(DESKTHING_EVENTS.START, start);
-
 DeskThing.on(DESKTHING_EVENTS.STOP, stop);
