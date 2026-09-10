b6e324991ba6402ef81ab00df4b5fb65926d1cc3
123-Game <aefoster74@gmail.com>
2026-09-11 00:07:18 +0100

    Added main webpage frame

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React from "react";
+import { useDeskThingData } from "./hooks/useDeskThingData";
+import { Header } from "./components/Header";
+import { HeroGlucose } from "./components/HeroGlucose";
+import { GlucoseChart } from "./components/GlucoseChart";
 
-const App: React.FC = () => {
+export const App: React.FC = () => {
+    const data = useDeskThingData();
 
     return (
-        <div className="bg-slate-900 gap-2 flex-col w-screen h-screen flex justify-center items-center">
-            <p className="font-bold text-5xl text-white">New DeskThing App</p>
-        </div>
+        <main className="fixed inset-0 w-full h-full bg-black text-white p-4 sm:p-6 flex flex-col justify-between overflow-hidden select-none font-sans">
+            <section className="flex-shrink-0">
+                <Header />
+            </section>
 
-    )
-}
+            <section className="flex-shrink-0">
+                <HeroGlucose data={data} />
+            </section>
 
-export default App
+            <section className="w-full flex-1 min-h-0 flex flex-col justify-end pt-2">
+                <GlucoseChart
+                    dataPoints={data.dataPoints}
+                    forecastPoints={data.forecastPoints}
+                    statusColor={data.statusColor}
+                    units={data.units}
+                />
+            </section>
+        </main>
+    );
+};
+
+export default App;
